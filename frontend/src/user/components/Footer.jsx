282a2c86f769ab9c3656185d4e53a9f2e6276037
Thisaru Rajapaksha <thisarumiuranga@gmail.com>
2025-02-25 01:19:45 +0530

    feat: footer

--- frontend/src/user/components/Footer.jsx
+++ frontend/src/user/components/Footer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
 
 export default function Footer() {
   return (
     <div>
-        Footer
+    <footer className="pt-22 px-6 md:px-20">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between">
+        {/* Left Section */}
+        <div className="mb-8 md:mb-0">
+          <h2 className="text-graylight font-bold text-lg">RENTACAR</h2>
+          <p className="mt-2 text-graydark max-w-xs">
+            Wherever you go, we make renting a car simple, fast, and affordable!
+          </p>
+        </div>
+
+        {/* Middle Section - Menu */}
+        <div className="mb-8 md:mb-0 sm:ml-[10rem] md:ml-[15rem]">
+          <h3 className="text-graylight font-semibold">Menu</h3>
+          <ul className="mt-2 text-graydark space-y-2">
+            <li><a href="#" className="hover:text-graylight">Home</a></li>
+            <li><a href="#" className="hover:text-graylight">Garage</a></li>
+            <li><a href="#" className="hover:text-graylight">About</a></li>
+            <li><a href="#" className="hover:text-graylight">Contact</a></li>
+          </ul>
+        </div>
+
+        {/* Right Section - Legal */}
+        <div>
+          <h3 className="text-graylight font-semibold">Legal</h3>
+          <ul className="mt-2 text-graydark space-y-2 md:pb-20">
+            <li><a href="#" className="hover:text-graylight">Terms of service</a></li>
+            <li><a href="#" className="hover:text-graylight">Privacy Policy</a></li>
+            <li><a href="#" className="hover:text-graylight">Cookie Policy</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Bottom Separator */}
+      <div className="mt-10 pb-6 md:pb-20 border-t border-graydark"></div>
+
+      {/*last Text */}
+    </footer>
+      <div className="text-[4.6rem] md:text-[18rem] font-extrabold leading-tight md:leading-50 flex justify-center items-center opacity-18 pointer-events-none select-none overflow-hidden">
+        RENTACAR
+      </div>
     </div>
-  )
+  );
 }
