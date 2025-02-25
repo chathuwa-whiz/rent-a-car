d289e6e7b27e0210da2bd533a39a8ad702539492
chathuwa-whiz <navodchathushka@gmail.com>
2025-02-25 11:48:08 +0530

    change header welcome text size

--- frontend/src/user/profile/Header.jsx
+++ frontend/src/user/profile/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center p-4 text-white">
-      <h1 className="text-lg font-semibold">Welcome Jake !</h1>
+      <h1 className="text-2xl font-md">Welcome Jake !</h1>
       {!isProfilePage && (
         <div className="flex gap-3 items-center">
           <span className="text-sm font-bold mr-2">Jake Danial</span>
