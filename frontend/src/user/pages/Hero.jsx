f2048428574d31fecd07455b80d697c7431f73d5
Thisaru Rajapaksha <thisarumiuranga@gmail.com>
2025-02-26 10:57:51 +0530

    improve responsiveness of Hero

--- frontend/src/user/pages/Hero.jsx
+++ frontend/src/user/pages/Hero.jsx
@@ -33,12 +33,12 @@ export default function Hero() {
       <div className="flex flex-row items-center justify-center md:justify-start gap-x-10 md:gap-x-16 w-full mt-20 md:mt-0 md:ml-4">
         
         {/* Social Icons */}
-        <div className="flex flex-col mt-4 md:mt-10 space-y-11 md:space-y-23 text-graydark">
-          <FaFacebookF className="text-lg md:text-xl cursor-pointer hover:text-graylight transition" />
-          <FaFacebookMessenger className="text-lg md:text-xl cursor-pointer hover:text-graylight transition" />
-          <FaInstagram className="text-lg md:text-xl cursor-pointer hover:text-graylight transition" />
-          <FaWhatsapp className="text-lg md:text-xl cursor-pointer hover:text-graylight transition" />
-          <FaEnvelope className="text-lg md:text-xl cursor-pointer hover:text-graylight transition" />
+        <div className="flex flex-col mt-4 xl:mt-10 space-y-11 md:space-y-13 lg:space-y-16 xl:space-y-23 text-graydark">
+          <FaFacebookF className="text-lg lg:text-xl cursor-pointer hover:text-graylight transition" />
+          <FaFacebookMessenger className="text-lg lg:text-xl cursor-pointer hover:text-graylight transition" />
+          <FaInstagram className="text-lg lg:text-xl cursor-pointer hover:text-graylight transition" />
+          <FaWhatsapp className="text-lg lg:text-xl cursor-pointer hover:text-graylight transition" />
+          <FaEnvelope className="text-lg lg:text-xl cursor-pointer hover:text-graylight transition" />
         </div>
 
         {/* Main Content with Animation */}
@@ -51,29 +51,29 @@ export default function Hero() {
             transition={{ duration: 0.8, ease: "easeInOut" }} // Smooth transition
             className="text-white text-left md:text-left"
           >
-            <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl mt-4 font-bold uppercase leading-none tracking-widest">
+            <h1 className="text-4xl sm:text-4xl md:text-5xl xl:text-8xl mt-4 font-bold uppercase leading-none tracking-widest">
               Luxury <br /> Lifestyle <br /> Rentals
             </h1>
 
             {/* Decorative Lines */}
-            <div className="flex justify-start gap-2 mt-4 mb-6">
-              <div className="w-16 sm:w-20 h-[6px] sm:h-[8px] bg-white rounded-full"></div>
-              <div className="w-4 sm:w-6 h-[6px] sm:h-[8px] bg-white rounded-full"></div>
-              <div className="w-4 sm:w-6 h-[6px] sm:h-[8px] bg-white rounded-full"></div>
-              <div className="w-12 sm:w-14 h-[6px] sm:h-[8px] bg-white rounded-full"></div>
+            <div className="flex justify-start gap-1 md:gap-2 mt-4 mb-6">
+              <div className="w-8 md:w-10 lg:w-20 h-[4px] md:h-[6px] lg:h-[8px] bg-white rounded-full"></div>
+              <div className="w-4 md:w-4 lg:w-6 h-[4px] md:h-[6px] lg:h-[8px] bg-white rounded-full"></div>
+              <div className="w-4 md:w-4 lg:w-6 h-[4px] md:h-[6px] lg:h-[8px] bg-white rounded-full"></div>
+              <div className="w-6 md:w-8 lg:w-14 h-[4px] md:h-[6px] lg:h-[8px] bg-white rounded-full"></div>
             </div>
 
             {/* Pricing Section (Auto-Switching Car Details) */}
             <div className="mt-2 transition-opacity duration-500 ease-in-out">
-              <p className="text-xl sm:text-2xl font-bold pb-2">{carData[currentCar].name}</p>
-              <p className="text-darkred text-lg sm:text-xl font-bold inline tracking-wider">
+              <p className="text-sm lg:text-2xl font-bold pb-2">{carData[currentCar].name}</p>
+              <p className="text-darkred text-sm lg:text-xl font-bold inline tracking-wider">
                 {carData[currentCar].price}
               </p>
-              <span className="text-sm sm:text-lg font-bold text-graylight md:text-graydark"> / Per Day</span>
+              <span className="text-sm lg:text-lg font-bold text-graylight "> / Per Day</span>
             </div>
 
             {/* Booking Button */}
-            <button className="mt-6 sm:mt-12 px-8 sm:px-12 py-2 sm:py-3 border-2 cursor-pointer border-gasolindark text-sm sm:text-lg font-semibold rounded-[2px] hover:bg-gasolinlight hover:border-gasolinlight transition">
+            <button className="mt-6 lg:mt-12 px-8 lg:px-12 py-2 lg:py-3 border-2 cursor-pointer border-gasolindark text-sm lg:text-lg font-semibold rounded-[2px] hover:bg-gasolinlight hover:border-gasolinlight transition">
               Book Now
             </button>
           </motion.div>
@@ -81,7 +81,7 @@ export default function Hero() {
       </div>
 
       {/* Car Image Wrapper with Framer Motion (Slide-in Effect) */}
-      <div className="absolute bottom-0 -md:bottom-10 md:-right-20 w-full md:w-[55rem] overflow-hidden">
+      <div className="absolute bottom-0 -md:bottom-10 md:-right-20 w-[20rem] sm:w-[25rem] md:w-[30rem] lg:w-[40rem] xl:w-[50rem] overflow-hidden">
         <AnimatePresence mode="wait">
           <motion.img
             key={currentCar}
