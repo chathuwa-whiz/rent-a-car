168dacec5526a3f6dbddfbdcb7ed544eccbc1fd6
Thisaru Rajapaksha <thisarumiuranga@gmail.com>
2025-02-26 12:37:17 +0530

    improve responsiveness of About

--- frontend/src/user/pages/AboutUs.jsx
+++ frontend/src/user/pages/AboutUs.jsx
@@ -10,31 +10,31 @@ export default function AboutUs() {
         <img
           src="/aboutus1.png"
           alt="Luxury Car"
-          className="w-full md:w-[60%] h-auto md:h-[420px] -mb-20"
+          className="w-full lg:w-[60%] h-auto lg:h-[300px] xl:h-[420px] -mb-20"
         />
 
         {/* "RENT A CAR" Banner */}
-        <div className="absolute top-[80%] md:top-[20%] right-[38%] md:right-[37.5%] w-[40%] md:w-[6%] h-[30%] md:h-[50%] 
-        bg-gradient-to-r from-gasolindark to-gasolinlight md:px-2 py-4 md:py-16 font-bold z-50">
-          <h3 className="text-xl md:text-3xl w-full md:w-[12rem] text-white pl-4 md:pl-14 font-extrabold leading-tight">
+        <div className="absolute top-[80%] lg:top-[20%] right-[38%] lg:right-[37.5%] w-[35%] lg:w-[6%] h-[30%] lg:h-[50%] text-center 
+        bg-gradient-to-r from-gasolindark to-gasolinlight lg:px-2 py-4 lg:py-12 xl:py-16 font-bold z-50">
+          <h3 className="text:sm sm:text-xl md:text-2xl xl:text-3xl lg:w-[8rem] xl:w-[10rem] text-white lg:pl-2 xl:pl-4 md:pt-6 lg:pt-0 font-extrabold leading-tight">
             RENT A CAR
           </h3>
         </div>
 
-        <div className="absolute top-[102%] md:top-[55%] right-[14%] md:right-[28%] text-gasolindark font-bold z-50">
-          <p className="text-sm md:text-lg flex items-center gap-1">
-            Follow us <ArrowRight className="w-4 h-4 md:w-5 md:h-5" />
+        <div className="absolute top-[102%] lg:top-[58%] xl:top-[55%] right-[10%] md:right-[24%] xl:right-[28%] text-gasolindark font-bold z-50">
+          <p className="text-sm lg:text-lg flex items-center gap-1">
+            Follow us <ArrowRight className="w-4 h-4 lg:w-5 lg:h-5" />
           </p>
         </div>
       </div>
 
       {/* Bottom Section - Content & Features */}
-      <div className="relative py-14 px-4 md:px-12">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="relative py-14 ">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
           
           {/* Left Side - Text Section */}
-          <div className="mt-16 md:mt-20 md:-ml-10">
-            <h2 className="text-2xl md:text-3xl text-white font-extrabold">
+          <div className="mt-20 md:mt-24 lg:mt-16 ml-6 md:ml-10 xl:mt-20 xl:-ml-10">
+            <h2 className="text-2xl xl:text-3xl text-white font-extrabold">
               LUXURY CAR RENTAL SRI LANKA
             </h2>
             <p className="text-graydark my-6 md:my-8 w-full md:w-md leading-relaxed text-sm md:text-base">
@@ -44,28 +44,28 @@ export default function AboutUs() {
             </p>
 
             {/* Features Section */}
-            <div className="grid grid-cols-2 md:flex md:flex-row text-graylight items-start gap-6 md:gap-20 mt-6 md:mt-12">
-              <div className="flex items-center gap-2 md:gap-4 text-sm md:text-base">
-                <i className="fas fa-road text-xl md:text-2xl"></i>
+            <div className="grid grid-cols-2 md:flex md:flex-row text-graylight items-start gap-6 md:gap-10 xl:gap-20 mt-6 xl:mt-12">
+              <div className="flex items-center gap-4 text-sm xl:text-base">
+                <i className="fas fa-road text-xl xl:text-2xl"></i>
                 <p className="font-semibold">MILEAGE <br /> UNLIMITED</p>
               </div>
-              <div className="flex items-center gap-2 md:gap-4 text-sm md:text-base">
-                <i className="fas fa-map-marker-alt text-xl md:text-2xl"></i>
+              <div className="flex items-center gap-4 text-sm xl:text-base">
+                <i className="fas fa-map-marker-alt text-xl xl:text-2xl"></i>
                 <p className="font-semibold">PICK UP <br /> SERVICE</p>
               </div>
-              <div className="flex items-center gap-2 md:gap-4 text-sm md:text-base">
-                <i className="fas fa-shipping-fast text-xl md:text-2xl"></i>
+              <div className="flex items-center gap-4 text-sm xl:text-base">
+                <i className="fas fa-shipping-fast text-xl xl:text-2xl"></i>
                 <p className="font-semibold">DELIVERY <br /> TO DOOR</p>
               </div>
             </div>
           </div>
 
           {/* Right Side - Car Interior Image */}
-          <div className="w-full md:w-[50%] md:h-[420px] flex justify-center md:absolute md:top-5 md:right-0">
+          <div className="w-full lg:w-[50%] lg:h-[420px] flex justify-center lg:absolute lg:top-5 lg:right-0">
             <img
               src="/aboutus2.jpg"
               alt="Car Interior"
-              className="w-full md:h-[420px]"
+              className="w-full h-max-[300px] md:h-max-[300px] lg:h-[350px] xl:h-[420px] "
             />
           </div>
         </div>
