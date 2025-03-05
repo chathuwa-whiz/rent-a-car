d2dece4976d0d3cd0905a804652875aed765c8dd
Thisaru Rajapaksha <thisarumiuranga@gmail.com>
2025-03-06 01:21:32 +0530

    intergrate Booking management page frontend and backend

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -8,13 +8,41 @@ import {
   getUserBookings
 } from "../controller/bookingController.js";
 
+import { authenticate, authorize } from "../middleware/AuthMiddleware.js";
+
 const router = express.Router();
 
-router.get("/", getAllBookings); 
-router.get("/user", getUserBookings);
-router.get("/:id", getBookingById);
-router.post("/", createBooking);
-router.put("/:id", updateBooking);
-router.delete("/:id", deleteBooking);
+router.get("/",
+  authenticate,
+  // authorize("admin"),
+  getAllBookings // Admin-only access
+);
+
+router.get("/user",
+  authenticate,
+  getUserBookings // Get user's own bookings
+);
+
+router.get("/:id",
+  authenticate,
+  getBookingById // Get a specific booking
+);
+
+router.post("/",
+  authenticate,
+  createBooking // Create a new booking (Only logged-in users)
+);
+
+router.put("/:id",
+  authenticate,
+  authorize("admin"),
+  updateBooking // Update booking status (Admin Only)
+);
+
+router.delete("/:id",
+  authenticate,
+  authorize("admin"),
+  deleteBooking // Delete a booking (Admin Only)
+);
 
 export default router;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,6 +3,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./services/authSlice";
 import { userApi } from "./services/userSlice";
 import { vehicleApi } from "./services/vehicleSlice"
+import { bookingApi } from "./services/bookingSlice"
+
 
 export const store = configureStore({
 
@@ -11,12 +13,14 @@ export const store = configureStore({
         [authApi.reducerPath]: authApi.reducer,
         [userApi.reducerPath]: userApi.reducer,
         [vehicleApi.reducerPath]: vehicleApi.reducer,
+        [bookingApi.reducerPath]: bookingApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
             .concat(authApi.middleware)
             .concat(vehicleApi.middleware)
-            .concat(userApi.middleware),
+            .concat(userApi.middleware)
+            .concat(bookingApi.middleware),
 
 })
