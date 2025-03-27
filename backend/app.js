436fd3ebbdd02d55ae1867e72e0b34d98629dc99
chathuwa-whiz <navodchathushka@gmail.com>
2025-03-27 11:32:28 +0530

    feat: add helmet middleware for enhanced security

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
 import dotenv from "dotenv";
 import vehicleRoutes from "./routes/vehicleRoutes.js";
@@ -18,6 +19,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
